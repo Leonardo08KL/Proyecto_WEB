3a554593f7af3b667dacfb5fa337c336f734467b
Leonardo08KL <95943337+Leonardo08KL@users.noreply.github.com>
2023-04-03 20:55:02 -0600

    first commit

---
+++ menu.php
@@ -0,0 +1,31 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.php">PONTE A CHAMBIAR</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarColor02">
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                    <a class="nav-link active" href="index.php">INICIO
+                        <span class="visually-hidden">(current)</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="SingUp.html">Registrarse</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="Login.html">Iniciar Sesi&oacute;n</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Acerca de Nosotros</a>
+                </li>
+            </ul>
+            <form class="d-flex">
+                <a class="btn btn-outline-light" href="perfil.php">
+                    PERFIL
+                </a>
+            </form>
+        </div>
+    </div>
+</nav>
---
+++ propuesta.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Proyecto WEB</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+    <script src='main.js'></script>
+</head>
+
+<body>
+
+    <?php
+    include "menu.php";
+    ?>
+
+
+    <header class="bg-dark py-5 mt-5 ms-5 me-5">
+        <div class="container px-4 px-lg-5 my-5">
+            <div class="text-center text-white">
+                <h1 class="display-4 fw-bolder">TRABAJO 1</h1>
+                <p class="lead fw-normal text-white-50 mb-0">¡Atr&eacute;vete a dar el primer paso hacia tu futuro laboral! Est&aacute;s a solo un paso de lograr el trabajo de tus sueños y nosotros estamos aqu&iacute; para ayudarte a conseguirlo.</p>
+            </div>
+        </div>
+    </header>
+
+</body>
+
+</html>
