e09bd1df293249c152fe8014133be0dc58cc5cb0
Leonardo08KL <95943337+Leonardo08KL@users.noreply.github.com>
2023-05-27 19:02:02 -0600

    Commite Final

--- CONSULTAR_DATOS_PERFIL/datos_perfil.php
+++ CONSULTAR_DATOS_PERFIL/datos_perfil.php
@@ -1,335 +1,112 @@
 <?php
-require('CONEXION/conexion.php');
+
+
+$sql = "";
+$connectionDB = new ConnectionDB();
 
 if (session_status() == PHP_SESSION_NONE) {
-
-    $sql = "SELECT c.correo,
-    u.nombre ,
-    u.A_paterno ,
-    u.A_materno ,
-    concat(u.Nombre,' ',u.A_paterno,' ',u.A_materno) AS NOMBRE_COMPLETO,
-    u.Nacimiento,
-    u.Telefono,
-    l.localidad,
-    m.municipio,
-    e.estado,
-    cg.genero,
-    u.Foto,
-    ne.institucion,
-    ne.fecha_ingreso,
-    ne.fecha_egreso,
-    ne.descripcion
-    from cuenta c
-    inner join usuario u on c.ID_usuario = u.ID_usuario
-    inner join catalogo_genero cg on u.ID_genero = cg.ID_genero
-    inner join localidad l on u.ID_localidad = l.ID_localidad
-    inner join municipio m on l.ID_municipio = m.ID_municipio
-    inner join estado e on m.ID_estado = e.ID_estado
-    inner join nivel_estudios ne on u.ID_usuario = ne.ID_usuario
-    inner join actividad_profesional ap on u.ID_usuario = ap.ID_usuario
-    where ap.ID_oferta = " . $_GET['ID'] . ";";
-
-    $sqlExperiencia = "SELECT el.ID_experiencia, el.Nombre_experiencia, el.Fecha_ingreso, el.Fecha_egreso, el.Descripcion
-    FROM experiencia_laboral el
-    INNER JOIN usuario u on el.ID_usuario = u.ID_usuario
-    INNER JOIN cuenta c on u.ID_usuario = c.ID_usuario
-    inner join actividad_profesional ap on u.ID_usuario = ap.ID_usuario
-    where ap.ID_oferta = " . $_GET['ID'] . ";";
-
-    $result = mysqli_query($conn, $sql);
-
-    $resultExperiencia = mysqli_query($conn, $sqlExperiencia);
-
-    if (!$result) {
-        die("Error en la consulta: " . mysqli_error($conn));
-    }
-    if (!$resultExperiencia) {
-        die("Error en la consulta: " . mysqli_error($conn));
-    }
-
-    // Obtener el resultado de la consulta
-    while ($row = mysqli_fetch_assoc($result)) {
-        // Acceder a los valores de cada fila
-        $correo = $row["correo"];
-        $nombre = $row["nombre"];
-        $a_paterno = $row["A_paterno"];
-        $a_materno = $row["A_materno"];
-        $nombre_completo = $row["NOMBRE_COMPLETO"];
-        $nacimiento = $row["Nacimiento"];
-        $telefono = $row["Telefono"];
-        $localidad = $row["localidad"];
-        $municipio = $row["municipio"];
-        $estado = $row["estado"];
-        $genero = $row["genero"];
-        $foto = $row["Foto"];
-        $institucion = $row["institucion"];
-        $fecha_ingreso = $row["fecha_ingreso"];
-        $fecha_egreso = $row["fecha_egreso"];
-        $descripcion = $row["descripcion"];
-    }
+    $NoCookies = " where ap.ID_oferta = " . $_GET['ID'] . ";";
 
 
-    /**
-    while ($row = mysqli_fetch_assoc($resultExperiencia)) {
-    // Acceder a los valores de cada fila
-    $Nombre_experiencia = $row['Nombre_experiencia'];
-    $fecha_ingresoExp = $row['Fecha_ingreso'];
-    $fecha_egresoExp = $row['Fecha_egreso'];
-    $descripcionExp = $row['Descripcion'];
-    }
-    
-    
-     */
-    function datos_actividad_profesional()
-    {
-        $sql = "SELECT c.correo,
-    u.nombre ,
-    u.A_paterno ,
-    u.A_materno ,
-    concat(u.Nombre,' ',u.A_paterno,' ',u.A_materno) AS NOMBRE_COMPLETO,
-    u.Nacimiento,
-    u.Telefono,
-    l.localidad,
-    m.municipio,
-    e.estado,
-    cg.genero,
-    u.Foto,
-    ne.institucion,
-    ne.fecha_ingreso,
-    ne.fecha_egreso,
-    ne.descripcion
-    from cuenta c
-    inner join usuario u on c.ID_usuario = u.ID_usuario
-    inner join catalogo_genero cg on u.ID_genero = cg.ID_genero
-    inner join localidad l on u.ID_localidad = l.ID_localidad
-    inner join municipio m on l.ID_municipio = m.ID_municipio
-    inner join estado e on m.ID_estado = e.ID_estado
-    inner join nivel_estudios ne on u.ID_usuario = ne.ID_usuario
-    inner join actividad_profesional ap on u.ID_usuario = ap.ID_usuario
-    where ap.ID_oferta = " . $_GET['ID'] . ";";
-    }
-    mysqli_close($conn);
+    $op = 1;
+    $sql = $connectionDB->queryConsulta($sql, $op) . $NoCookies;
+    $result = $connectionDB->conexion($sql);
+
+    list($correo, $nombre, $a_paterno, $a_materno, $nombre_completo, $nacimiento, $telefono, $localidad, $municipio, $estado, $genero, $foto, $institucion, $fecha_ingreso, $fecha_egreso, $descripcion) = $connectionDB->getDatos($result);
+
+
+    $op = 2;
+    $sql = $connectionDB->queryConsulta($sql, $op) . $NoCookies;
+    $resultExperiencia = $connectionDB->conexion($sql);
 } else {
 
     $email = $_SESSION['correo'];
     if (isset($_GET['ID'])) {
+        $NoCookies = " where ap.ID_oferta = " . $_GET['ID'] . ";";
+
+        $op = 1;
+        $sql = $connectionDB->queryConsulta($sql, $op) . $NoCookies;
+        $result = $connectionDB->conexion($sql);
+
+        list($correo, $nombre, $a_paterno, $a_materno, $nombre_completo, $nacimiento, $telefono, $localidad, $municipio, $estado, $genero, $foto, $institucion, $fecha_ingreso, $fecha_egreso, $descripcion) = $connectionDB->getDatos($result);
 
 
-        $sql = "SELECT c.correo,
-    u.nombre ,
-    u.A_paterno ,
-    u.A_materno ,
-    concat(u.Nombre,' ',u.A_paterno,' ',u.A_materno) AS NOMBRE_COMPLETO,
-    u.Nacimiento,
-    u.Telefono,
-    l.localidad,
-    m.municipio,
-    e.estado,
-    cg.genero,
-    u.Foto,
-    ne.institucion,
-    ne.fecha_ingreso,
-    ne.fecha_egreso,
-    ne.descripcion
-    from cuenta c
-    inner join usuario u on c.ID_usuario = u.ID_usuario
-    inner join catalogo_genero cg on u.ID_genero = cg.ID_genero
-    inner join localidad l on u.ID_localidad = l.ID_localidad
-    inner join municipio m on l.ID_municipio = m.ID_municipio
-    inner join estado e on m.ID_estado = e.ID_estado
-    inner join nivel_estudios ne on u.ID_usuario = ne.ID_usuario
-    inner join actividad_profesional ap on u.ID_usuario = ap.ID_usuario
-    where ap.ID_oferta = " . $_GET['ID'] . ";";
-
-        $sqlExperiencia = "SELECT el.ID_experiencia, el.Nombre_experiencia, el.Fecha_ingreso, el.Fecha_egreso, el.Descripcion
-    FROM experiencia_laboral el
-    INNER JOIN usuario u on el.ID_usuario = u.ID_usuario
-    INNER JOIN cuenta c on u.ID_usuario = c.ID_usuario
-    inner join actividad_profesional ap on u.ID_usuario = ap.ID_usuario
-    where ap.ID_oferta = " . $_GET['ID'] . ";";
-
-        $result = mysqli_query($conn, $sql);
-
-        $resultExperiencia = mysqli_query($conn, $sqlExperiencia);
-
-        if (!$result) {
-            die("Error en la consulta: " . mysqli_error($conn));
-        }
-        if (!$resultExperiencia) {
-            die("Error en la consulta: " . mysqli_error($conn));
-        }
-
-        // Obtener el resultado de la consulta
-        while ($row = mysqli_fetch_assoc($result)) {
-            // Acceder a los valores de cada fila
-            $correo = $row["correo"];
-            $nombre = $row["nombre"];
-            $a_paterno = $row["A_paterno"];
-            $a_materno = $row["A_materno"];
-            $nombre_completo = $row["NOMBRE_COMPLETO"];
-            $nacimiento = $row["Nacimiento"];
-            $telefono = $row["Telefono"];
-            $localidad = $row["localidad"];
-            $municipio = $row["municipio"];
-            $estado = $row["estado"];
-            $genero = $row["genero"];
-            $foto = $row["Foto"];
-            $institucion = $row["institucion"];
-            $fecha_ingreso = $row["fecha_ingreso"];
-            $fecha_egreso = $row["fecha_egreso"];
-            $descripcion = $row["descripcion"];
-        }
-
-
-        /**
-    while ($row = mysqli_fetch_assoc($resultExperiencia)) {
-    // Acceder a los valores de cada fila
-    $Nombre_experiencia = $row['Nombre_experiencia'];
-    $fecha_ingresoExp = $row['Fecha_ingreso'];
-    $fecha_egresoExp = $row['Fecha_egreso'];
-    $descripcionExp = $row['Descripcion'];
-    }
-    
-    
-         */
-        function datos_actividad_profesional()
-        {
-            $sql = "SELECT c.correo,
-    u.nombre ,
-    u.A_paterno ,
-    u.A_materno ,
-    concat(u.Nombre,' ',u.A_paterno,' ',u.A_materno) AS NOMBRE_COMPLETO,
-    u.Nacimiento,
-    u.Telefono,
-    l.localidad,
-    m.municipio,
-    e.estado,
-    cg.genero,
-    u.Foto,
-    ne.institucion,
-    ne.fecha_ingreso,
-    ne.fecha_egreso,
-    ne.descripcion
-    from cuenta c
-    inner join usuario u on c.ID_usuario = u.ID_usuario
-    inner join catalogo_genero cg on u.ID_genero = cg.ID_genero
-    inner join localidad l on u.ID_localidad = l.ID_localidad
-    inner join municipio m on l.ID_municipio = m.ID_municipio
-    inner join estado e on m.ID_estado = e.ID_estado
-    inner join nivel_estudios ne on u.ID_usuario = ne.ID_usuario
-    inner join actividad_profesional ap on u.ID_usuario = ap.ID_usuario
-    where ap.ID_oferta = " . $_GET['ID'] . ";";
-        }
-        mysqli_close($conn);
+        $op = 2;
+        $sql = $connectionDB->queryConsulta($sql, $op) . $NoCookies;
+        $resultExperiencia = $connectionDB->conexion($sql);
     } else {
+        $SiCokies = " WHERE c.correo = '" . $_SESSION['correo'] . "';";
+
+        $op = 1;
+        $sql = $connectionDB->queryConsulta($sql, $op) . $SiCokies;
+        $result = $connectionDB->conexion($sql);
+
+        list($correo, $nombre, $a_paterno, $a_materno, $nombre_completo, $nacimiento, $telefono, $localidad, $municipio, $estado, $genero, $foto, $institucion, $fecha_ingreso, $fecha_egreso, $descripcion) = $connectionDB->getDatos($result);
 
 
-
-
-        require('CONEXION/conexion.php');
-
-        $sql = "SELECT c.correo,
-                    u.nombre ,
-                    u.A_paterno ,
-                    u.A_materno ,
-                    concat(u.Nombre,' ',u.A_paterno,' ',u.A_materno) AS NOMBRE_COMPLETO,
-                    u.Nacimiento,
-                    u.Telefono,
-                    l.localidad,
-                    m.municipio,
-                    e.estado,
-                    cg.genero,
-                    u.Foto,
-                    ne.institucion,
-                    ne.fecha_ingreso,
-                    ne.fecha_egreso,
-                    ne.descripcion
-                from cuenta c
-                    inner join usuario u on c.ID_usuario = u.ID_usuario
-                    inner join catalogo_genero cg on u.ID_genero = cg.ID_genero
-                    inner join localidad l on u.ID_localidad = l.ID_localidad
-                    inner join municipio m on l.ID_municipio = m.ID_municipio
-                    inner join estado e on m.ID_estado = e.ID_estado
-                    inner join nivel_estudios ne on u.ID_usuario = ne.ID_usuario
-                where correo = '" . $email . "';";
-
-        $sqlExperiencia = "SELECT el.ID_experiencia, el.Nombre_experiencia, el.Fecha_ingreso, el.Fecha_egreso, el.Descripcion
-                            FROM experiencia_laboral el
-                        INNER JOIN usuario u on el.ID_usuario = u.ID_usuario
-                        INNER JOIN cuenta c on u.ID_usuario = c.ID_usuario
-                        WHERE c.correo = '" . $_SESSION['correo'] . "';";
-
-        $result = mysqli_query($conn, $sql);
-
-        $resultExperiencia = mysqli_query($conn, $sqlExperiencia);
-
-        if (!$result) {
-            die("Error en la consulta: " . mysqli_error($conn));
-        }
-        if (!$resultExperiencia) {
-            die("Error en la consulta: " . mysqli_error($conn));
-        }
-
-        // Obtener el resultado de la consulta
-        while ($row = mysqli_fetch_assoc($result)) {
-            // Acceder a los valores de cada fila
-            $correo = $row["correo"];
-            $nombre = $row["nombre"];
-            $a_paterno = $row["A_paterno"];
-            $a_materno = $row["A_materno"];
-            $nombre_completo = $row["NOMBRE_COMPLETO"];
-            $nacimiento = $row["Nacimiento"];
-            $telefono = $row["Telefono"];
-            $localidad = $row["localidad"];
-            $municipio = $row["municipio"];
-            $estado = $row["estado"];
-            $genero = $row["genero"];
-            $foto = $row["Foto"];
-            $institucion = $row["institucion"];
-            $fecha_ingreso = $row["fecha_ingreso"];
-            $fecha_egreso = $row["fecha_egreso"];
-            $descripcion = $row["descripcion"];
-        }
-
-
-        /**
-     while ($row = mysqli_fetch_assoc($resultExperiencia)) {
-        // Acceder a los valores de cada fila
-        $Nombre_experiencia = $row['Nombre_experiencia'];
-        $fecha_ingresoExp = $row['Fecha_ingreso'];
-        $fecha_egresoExp = $row['Fecha_egreso'];
-        $descripcionExp = $row['Descripcion'];
-    }
-
-
-         */
-        function datos_actividad_profesional()
-        {
-            $sql = "SELECT c.correo,
-        u.nombre ,
-        u.A_paterno ,
-        u.A_materno ,
-        concat(u.Nombre,' ',u.A_paterno,' ',u.A_materno) AS NOMBRE_COMPLETO,
-        u.Nacimiento,
-        u.Telefono,
-        l.localidad,
-        m.municipio,
-        e.estado,
-        cg.genero,
-        u.Foto,
-        ne.institucion,
-        ne.fecha_ingreso,
-        ne.fecha_egreso,
-        ne.descripcion
-    from cuenta c
-        inner join usuario u on c.ID_usuario = u.ID_usuario
-        inner join catalogo_genero cg on u.ID_genero = cg.ID_genero
-        inner join localidad l on u.ID_localidad = l.ID_localidad
-        inner join municipio m on l.ID_municipio = m.ID_municipio
-        inner join estado e on m.ID_estado = e.ID_estado
-        inner join nivel_estudios ne on u.ID_usuario = ne.ID_usuario
-    where correo = '" . $_SESSION['correo'] . "';";
-        }
-        mysqli_close($conn);
+        $op = 2;
+        $sql = $connectionDB->queryConsulta($sql, $op) . $SiCokies;
+        $resultExperiencia = $connectionDB->conexion($sql);
     }
 }
+
+?>
+
+
+<?php
+
+class ConnectionDB
+{
+
+    function queryConsulta($sql, $op)
+    {
+        if ($op == 1) {
+            $sql = "SELECT c.correo, u.nombre , u.A_paterno , u.A_materno , concat(u.Nombre,' ',u.A_paterno,' ',u.A_materno) AS NOMBRE_COMPLETO, u.Nacimiento, u.Telefono, l.localidad, m.municipio, e.estado, cg.genero, u.Foto, ne.institucion, ne.fecha_ingreso, ne.fecha_egreso, ne.descripcion from cuenta c inner join usuario u on c.ID_usuario = u.ID_usuario inner join catalogo_genero cg on u.ID_genero = cg.ID_genero inner join localidad l on u.ID_localidad = l.ID_localidad inner join municipio m on l.ID_municipio = m.ID_municipio inner join estado e on m.ID_estado = e.ID_estado inner join nivel_estudios ne on u.ID_usuario = ne.ID_usuario inner join actividad_profesional ap on u.ID_usuario = ap.ID_usuario";
+        } elseif ($op == 2) {
+
+            $sql = "SELECT el.ID_experiencia, el.Nombre_experiencia, el.Fecha_ingreso, el.Fecha_egreso, el.Descripcion
+            FROM experiencia_laboral el
+            INNER JOIN usuario u on el.ID_usuario = u.ID_usuario
+            INNER JOIN cuenta c on u.ID_usuario = c.ID_usuario
+            inner join actividad_profesional ap on u.ID_usuario = ap.ID_usuario";
+        }
+        return $sql;
+    }
+
+
+    function conexion($sql)
+    {
+        require('CONEXION/conexion.php');
+        $result = mysqli_query($conn, $sql);
+        if (!$result) {
+            die("Error en la consulta: " . mysqli_error($conn));
+        }
+        return $result;
+    }
+
+    public function getDatos($result)
+    {
+        while ($row = mysqli_fetch_assoc($result)) {
+            // Acceder a los valores de cada fila
+            $correo = $row["correo"];
+            $nombre = $row["nombre"];
+            $a_paterno = $row["A_paterno"];
+            $a_materno = $row["A_materno"];
+            $nombre_completo = $row["NOMBRE_COMPLETO"];
+            $nacimiento = $row["Nacimiento"];
+            $telefono = $row["Telefono"];
+            $localidad = $row["localidad"];
+            $municipio = $row["municipio"];
+            $estado = $row["estado"];
+            $genero = $row["genero"];
+            $foto = $row["Foto"];
+            $institucion = $row["institucion"];
+            $fecha_ingreso = $row["fecha_ingreso"];
+            $fecha_egreso = $row["fecha_egreso"];
+            $descripcion = $row["descripcion"];
+        }
+
+        return array($correo, $nombre, $a_paterno, $a_materno, $nombre_completo, $nacimiento, $telefono, $localidad, $municipio, $estado, $genero, $foto, $institucion, $fecha_ingreso, $fecha_egreso, $descripcion);
+    }
+}
+?>
--- menu.php
+++ menu.php
@@ -18,7 +18,7 @@ if (isset($_COOKIE["sessionID"])) {
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="index.php">PONTE A CHAMBIAR</a>
+        <a class="navbar-brand" href="index.php">JobConnect</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
