241ceeaa1917f9892d7164fcdf7767eb8f21648a
Leonardo08KL <95943337+Leonardo08KL@users.noreply.github.com>
2023-06-02 12:54:42 -0600

    Ahora Si ultimo

--- CONSULTAR_DATOS_PERFIL/datos_perfil.php
+++ CONSULTAR_DATOS_PERFIL/datos_perfil.php
@@ -1,25 +1,17 @@
 <?php
-
-
 $sql = "";
 $connectionDB = new ConnectionDB();
 
 if (session_status() == PHP_SESSION_NONE) {
     $NoCookies = " where ap.ID_oferta = " . $_GET['ID'] . ";";
-
-
     $op = 1;
     $sql = $connectionDB->queryConsulta($sql, $op) . $NoCookies;
     $result = $connectionDB->conexion($sql);
-
     list($correo, $nombre, $a_paterno, $a_materno, $nombre_completo, $nacimiento, $telefono, $localidad, $municipio, $estado, $genero, $foto, $institucion, $fecha_ingreso, $fecha_egreso, $descripcion) = $connectionDB->getDatos($result);
-
-
     $op = 2;
     $sql = $connectionDB->queryConsulta($sql, $op) . $NoCookies;
     $resultExperiencia = $connectionDB->conexion($sql);
 } else {
-
     $email = $_SESSION['correo'];
     if (isset($_GET['ID'])) {
         $NoCookies = " where ap.ID_oferta = " . $_GET['ID'] . ";";
@@ -36,14 +28,11 @@ if (session_status() == PHP_SESSION_NONE) {
         $resultExperiencia = $connectionDB->conexion($sql);
     } else {
         $SiCokies = " WHERE c.correo = '" . $_SESSION['correo'] . "';";
-
-        $op = 1;
+        $op = 3;
         $sql = $connectionDB->queryConsulta($sql, $op) . $SiCokies;
         $result = $connectionDB->conexion($sql);
 
         list($correo, $nombre, $a_paterno, $a_materno, $nombre_completo, $nacimiento, $telefono, $localidad, $municipio, $estado, $genero, $foto, $institucion, $fecha_ingreso, $fecha_egreso, $descripcion) = $connectionDB->getDatos($result);
-
-
         $op = 2;
         $sql = $connectionDB->queryConsulta($sql, $op) . $SiCokies;
         $resultExperiencia = $connectionDB->conexion($sql);
@@ -69,6 +58,8 @@ class ConnectionDB
             INNER JOIN usuario u on el.ID_usuario = u.ID_usuario
             INNER JOIN cuenta c on u.ID_usuario = c.ID_usuario
             inner join actividad_profesional ap on u.ID_usuario = ap.ID_usuario";
+        } elseif($op == 3){
+            $sql = "SELECT c.correo, u.nombre , u.A_paterno , u.A_materno , concat(u.Nombre,' ',u.A_paterno,' ',u.A_materno) AS NOMBRE_COMPLETO, u.Nacimiento, u.Telefono, l.localidad, m.municipio, e.estado, cg.genero, u.Foto, ne.institucion, ne.fecha_ingreso, ne.fecha_egreso, ne.descripcion from cuenta c inner join usuario u on c.ID_usuario = u.ID_usuario inner join catalogo_genero cg on u.ID_genero = cg.ID_genero inner join localidad l on u.ID_localidad = l.ID_localidad inner join municipio m on l.ID_municipio = m.ID_municipio inner join estado e on m.ID_estado = e.ID_estado inner join nivel_estudios ne on u.ID_usuario = ne.ID_usuario";
         }
         return $sql;
     }
@@ -84,10 +75,9 @@ class ConnectionDB
         return $result;
     }
 
-    public function getDatos($result)
+    function getDatos($result)
     {
         while ($row = mysqli_fetch_assoc($result)) {
-            // Acceder a los valores de cada fila
             $correo = $row["correo"];
             $nombre = $row["nombre"];
             $a_paterno = $row["A_paterno"];
@@ -105,7 +95,6 @@ class ConnectionDB
             $fecha_egreso = $row["fecha_egreso"];
             $descripcion = $row["descripcion"];
         }
-
         return array($correo, $nombre, $a_paterno, $a_materno, $nombre_completo, $nacimiento, $telefono, $localidad, $municipio, $estado, $genero, $foto, $institucion, $fecha_ingreso, $fecha_egreso, $descripcion);
     }
 }
--- perfil.php
+++ perfil.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html>
-
 <head>
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
@@ -9,17 +8,15 @@
 
     <!--CSS BOOTSTRAP -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
-    <link rel='stylesheet' type='text/css' media='screen' href='./styles/indexStyle.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
     <script src='main.js'></script>
 </head>
 
 <body>
-    <?php
+<?php
     include "menu.php";
-    ?>
-
+?>
     <?php
-
     require('CONSULTAR_DATOS_PERFIL/datos_perfil.php');
     ?>
     <div class="container mt-5 mb-5 card">
